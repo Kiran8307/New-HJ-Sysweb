86911635c3d43d45cfe4c18f6b08277d4b0fc19d
kiran-mistry-webdev <YOUR-OWNER-EMAIL>
2026-04-22 11:38:54 +0530

    Major performance optimization: Implement React.lazy and Suspense for code splitting, Add preconnect for fonts

--- src/App.js
+++ src/App.js
@@ -5,25 +5,26 @@ import { Helmet } from "react-helmet-async";
 import RootLayout from "./layouts/RootLayout";
 
 import Main from "./pages/Main";
-import AboutUs from "./pages/AboutUs";
-import ContactUs from "./pages/ContactUs";
-import AuditPage from "./pages/Audit";
-import IndustriesPage from "./pages/Industries";
-import CareerPage from "./pages/Career";
-import PortfolioPage from "./pages/Portfolio";
-import CaseStudiesPage from "./pages/CaseStudies";
-import CaseStudySingle from "./pages/CaseStudySingle";
-import ServicesPage from "./pages/Services";
-import ServiceSinglePage from "./pages/ServiceSingle";
-import BlogListPage from "./pages/BlogListPage";
-import BlogDetailPage from "./pages/BlogDetailPage";
-import ScrollToTop from "./components/ScrollToTop";
-import NotFound from "./pages/NotFound";
-import ThankYouPage from "./pages/ThankYouPage";
-import SubService from "./pages/SubService";
-import LocationPage from "./components/Locationcites/LocationPage";
 
-import CityServiceDetail from "./components/Locationcites/CityServiceDetail";
+// Lazy load other non-critical routes to reduce main JS bundle size
+const AboutUs = React.lazy(() => import("./pages/AboutUs"));
+const ContactUs = React.lazy(() => import("./pages/ContactUs"));
+const AuditPage = React.lazy(() => import("./pages/Audit"));
+const IndustriesPage = React.lazy(() => import("./pages/Industries"));
+const CareerPage = React.lazy(() => import("./pages/Career"));
+const PortfolioPage = React.lazy(() => import("./pages/Portfolio"));
+const CaseStudiesPage = React.lazy(() => import("./pages/CaseStudies"));
+const CaseStudySingle = React.lazy(() => import("./pages/CaseStudySingle"));
+const ServicesPage = React.lazy(() => import("./pages/Services"));
+const ServiceSinglePage = React.lazy(() => import("./pages/ServiceSingle"));
+const BlogListPage = React.lazy(() => import("./pages/BlogListPage"));
+const BlogDetailPage = React.lazy(() => import("./pages/BlogDetailPage"));
+const ScrollToTop = React.lazy(() => import("./components/ScrollToTop"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
+const ThankYouPage = React.lazy(() => import("./pages/ThankYouPage"));
+const SubService = React.lazy(() => import("./pages/SubService"));
+const LocationPage = React.lazy(() => import("./components/Locationcites/LocationPage"));
+const CityServiceDetail = React.lazy(() => import("./components/Locationcites/CityServiceDetail"));
 
 const SITE_URL = "https://www.hjsysweb.com";
 
@@ -52,32 +53,34 @@ export default function App() {
       </Helmet>
 
       <RootLayout>
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/about-us" element={<AboutUs />} />
-          <Route path="/contact-us" element={<ContactUs />} />
-          <Route path="/audit" element={<AuditPage />} />
-          <Route path="/industries-we-serve" element={<IndustriesPage />} />
-          <Route path="/career" element={<CareerPage />} />
-          <Route path="/portfolio" element={<PortfolioPage />} />
-          <Route path="/case-studies" element={<CaseStudiesPage />} />
-          <Route path="/case-studies/:slug" element={<CaseStudySingle />} />
-          <Route path="/services" element={<ServicesPage />} />
-          <Route path="/services/:id" element={<ServiceSinglePage />} />
-          <Route path="/services/:id/:subId" element={<SubService />} />
-          <Route path="/blogs" element={<BlogListPage />} />
-          <Route path="/blogs/:slug" element={<BlogDetailPage />} />
-          <Route path="/thank-you" element={<ThankYouPage />} />
-          <Route path="*" element={<NotFound />} />
+        <React.Suspense fallback={<div style={{minHeight: '100vh', display: 'flex', background: '#0a0a0a'}}></div>}>
+          <ScrollToTop />
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/about-us" element={<AboutUs />} />
+            <Route path="/contact-us" element={<ContactUs />} />
+            <Route path="/audit" element={<AuditPage />} />
+            <Route path="/industries-we-serve" element={<IndustriesPage />} />
+            <Route path="/career" element={<CareerPage />} />
+            <Route path="/portfolio" element={<PortfolioPage />} />
+            <Route path="/case-studies" element={<CaseStudiesPage />} />
+            <Route path="/case-studies/:slug" element={<CaseStudySingle />} />
+            <Route path="/services" element={<ServicesPage />} />
+            <Route path="/services/:id" element={<ServiceSinglePage />} />
+            <Route path="/services/:id/:subId" element={<SubService />} />
+            <Route path="/blogs" element={<BlogListPage />} />
+            <Route path="/blogs/:slug" element={<BlogDetailPage />} />
+            <Route path="/thank-you" element={<ThankYouPage />} />
+            <Route path="*" element={<NotFound />} />
+            
+            {/* CITY ROUTES */}
+            <Route path="/location/:cityId" element={<LocationPage />} />
           
-          {/* CITY ROUTES */}
-          <Route path="/location/:cityId" element={<LocationPage />} />
-        
-          {/* All city Services  */}
-          <Route path="/location/:cityId/:serviceId" element={<CityServiceDetail />} />
-          
-        </Routes>
+            {/* All city Services  */}
+            <Route path="/location/:cityId/:serviceId" element={<CityServiceDetail />} />
+            
+          </Routes>
+        </React.Suspense>
       </RootLayout>
     </>
   );
